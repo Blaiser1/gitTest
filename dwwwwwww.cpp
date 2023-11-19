e08b7bf79c001f152d7726e610acb7880261812e
Blaiser1 <bde606@gmail.com>
2023-11-19 23:59:50 +0700

    новый файл

---
+++ dwwwwwww.cpp
@@ -0,0 +1,6 @@
+#include "dwwwwwww.h"
+
+dwwwwwww::dwwwwwww()
+{
+
+}
---
+++ dwwwwwww.h
@@ -0,0 +1,11 @@
+#ifndef DWWWWWWW_H
+#define DWWWWWWW_H
+
+
+class dwwwwwww
+{
+public:
+    dwwwwwww();
+};
+
+#endif // DWWWWWWW_H
